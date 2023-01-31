39460d9fc0292503ac9a4479f52e5aaa2a9d08a0
gjlee <gjlee@openit.co.kr>
2023-01-31 17:31:40 +0900

    feat : typescript_dreamcoding motion 실습과제 init

---
+++ build/main.js
@@ -0,0 +1,19 @@
+"use strict";
+const btnList = document.querySelectorAll('.buttons button');
+if (btnList && btnList.length > 0) {
+    btnList.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            const target = e.target.value;
+            console.log(`click ! : ${target}`);
+        });
+    });
+}
+// function btnClick(type: BtnType): void {
+//   console.log(`click btn : ${type}`);
+//   // 버튼을 클릭할 시 입력 창 오픈
+//   const popup: HTMLElement | null = document.querySelector('div.popup');
+//   if (popup) {
+//     popup.style.display = 'block';
+//   }
+// }
+//# sourceMappingURL=main.js.map
