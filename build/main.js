bdd54dfea9054d097b659f45d7b1eacb805000b4
gjlee <gyujin89@gmail.com>
2023-02-01 15:12:09 +0900

    feat : 팝업 추가

--- build/main.js
+++ build/main.js
@@ -1,19 +1,44 @@
 "use strict";
 const btnList = document.querySelectorAll('.buttons button');
+const popup = document.querySelector('.popup');
+const dimmed = document.querySelector('.dimmed');
+const btnClose = document.querySelector('.close__button');
+const btnAdd = document.querySelector('.add__button');
 if (btnList && btnList.length > 0) {
     btnList.forEach((btn) => {
+        /**
+         * 상단 버튼들 이벤트 바인딩
+         */
         btn.addEventListener('click', (e) => {
-            const target = e.target.value;
-            console.log(`click ! : ${target}`);
+            const value = e.target.value;
+            popup.style.display = 'block';
+            dimmed.style.display = 'block';
         });
     });
 }
-// function btnClick(type: BtnType): void {
-//   console.log(`click btn : ${type}`);
-//   // 버튼을 클릭할 시 입력 창 오픈
-//   const popup: HTMLElement | null = document.querySelector('div.popup');
-//   if (popup) {
-//     popup.style.display = 'block';
-//   }
-// }
+/**
+ * 팝업 닫기 버튼 이벤트 바인딩
+ */
+btnClose.addEventListener('click', () => {
+    closePopup();
+});
+/**
+ * 팝업 내 추가 버튼 이벤트 바인딩
+ */
+btnAdd.addEventListener('click', () => {
+    addContents();
+});
+/**
+ * 팝업 닫기
+ */
+function closePopup() {
+    popup.style.display = 'none';
+    dimmed.style.display = 'none';
+}
+/**
+ * 팝업 내 추가 버튼 클릭 시 이벤트
+ */
+function addContents() {
+    closePopup();
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,53 @@
-type BtnType = 'image' | 'video' | 'note' | 'task' | null;
+type BtnType = 'image' | 'video' | 'note' | 'task';
 
 const btnList = document.querySelectorAll('.buttons button');
+const popup: HTMLElement = document.querySelector('.popup') as HTMLElement;
+const dimmed: HTMLElement = document.querySelector('.dimmed') as HTMLElement;
+const btnClose: HTMLElement = document.querySelector(
+  '.close__button'
+) as HTMLElement;
+const btnAdd: HTMLElement = document.querySelector(
+  '.add__button'
+) as HTMLElement;
 
 if (btnList && btnList.length > 0) {
   btnList.forEach((btn) => {
+    /**
+     * 상단 버튼들 이벤트 바인딩
+     */
     btn.addEventListener('click', (e) => {
-      const target = e.target.value;
-      console.log(`click ! : ${target}`);
+      const value = (e.target as HTMLButtonElement).value;
+      popup.style.display = 'block';
+      dimmed.style.display = 'block';
     });
   });
 }
 
-// function btnClick(type: BtnType): void {
-//   console.log(`click btn : ${type}`);
+/**
+ * 팝업 닫기 버튼 이벤트 바인딩
+ */
+btnClose.addEventListener('click', () => {
+  closePopup();
+});
 
-//   // 버튼을 클릭할 시 입력 창 오픈
-//   const popup: HTMLElement | null = document.querySelector('div.popup');
+/**
+ * 팝업 내 추가 버튼 이벤트 바인딩
+ */
+btnAdd.addEventListener('click', () => {
+  addContents();
+});
 
-//   if (popup) {
-//     popup.style.display = 'block';
-//   }
-// }
+/**
+ * 팝업 닫기
+ */
+function closePopup(): void {
+  popup.style.display = 'none';
+  dimmed.style.display = 'none';
+}
+
+/**
+ * 팝업 내 추가 버튼 클릭 시 이벤트
+ */
+function addContents() {
+  closePopup();
+}
